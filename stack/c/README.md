564a697ce43538d3189fe6e607fdadafb266c2c4
Adriano Perlini <aperlini@protonmail.ch>
2022-04-12 12:13:02 +0200

    upcoming stack in c

---
+++ stack/c/README.md
@@ -0,0 +1,13 @@
+## Usage
+
+To create the executable file *./bin/stack* : 
+
+```bash
+make
+```
+
+To delete all object files and the executable : 
+
+```bash
+make clean
+```
---
+++ stack/c/src/Stack.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+* * to be implementated : 
+* 
+* push(item)
+* pop()
+* peek()
+* isEmpty()
+* printStack()
+*
+*/
+
+
+
+int main() {
+    printf("stack implementation  \n");
+}
